e95b416e109a7524abaef4aab90711dbcaf382ba
pelitoflow <luisman2510@hotmail.com>
2023-11-13 23:12:15 -0500

    se agrego el boton de agregar y se actualizo el js para que fuera una ventana modal

--- src/main/resources/static/accion.js
+++ src/main/resources/static/accion.js
@@ -1,10 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    // Selección de botón "Agregar Usuario" por ID
+    const abrirModalBtn = document.getElementById('abrirModalBtn');
+    const cerrarModal = document.getElementById('cerrarModal');
+    const miModal = document.getElementById('miModal');
+
+    // Abrir el modal al hacer clic en el botón "Agregar Usuario"
+    abrirModalBtn.addEventListener('click', function () {
+        // Limpiar el formulario al abrir el modal
+        document.getElementById('formularioUsuario').reset();
+
+        miModal.style.display = 'block';
+    });
+
+    // Cerrar el modal al hacer clic en el botón de cerrar
+    cerrarModal.addEventListener('click', function () {
+        miModal.style.display = 'none';
+    });
+
     // Selección de botones "Actualizar" por clase
     const actualizarButtons = document.querySelectorAll(".actualizar-button");
 
     // Agregar un manejador de eventos a cada botón "Actualizar"
-    actualizarButtons.forEach(function(button) {
-        button.addEventListener("click", function() {
+    actualizarButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
             // Obtener el ID del cliente desde el atributo data-client-id
             const clientId = button.getAttribute("data-client-id");
 
@@ -17,8 +35,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const eliminarButtons = document.querySelectorAll(".eliminar-button");
 
     // Agregar un manejador de eventos a cada botón "Eliminar"
-    eliminarButtons.forEach(function(button) {
-        button.addEventListener("click", function() {
+    eliminarButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
             // Obtener el ID del cliente desde el atributo data-client-id
             const clientId = button.getAttribute("data-client-id");
 
@@ -28,5 +46,48 @@ document.addEventListener("DOMContentLoaded", function() {
                 window.location.href = "/borrar/" + clientId;
             }
         });
+    });
+
+    // Agregar un manejador de eventos al formulario dentro del modal
+    document.getElementById('formularioUsuario').addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const modalId = document.getElementById('modalId').value;
+        const modalNombre = document.getElementById('modalNombre').value;
+        const modalApellidos = document.getElementById('modalApellidos').value;
+        const modalEmail = document.getElementById('modalEmail').value;
+        const modalCiudad = document.getElementById('modalCiudad').value;
+        const modalTelefono = document.getElementById('modalTelefono').value;
+
+        // Construir el objeto con los datos del formulario
+        const usuarioData = {
+            id: modalId,
+            nombres: modalNombre,
+            apellidos: modalApellidos,
+            email: modalEmail,
+            ciudad: modalCiudad,
+            telefono: modalTelefono
+            // Agrega otros campos según sea necesario
+        };
+
+        // Realizar la solicitud POST utilizando Fetch API
+        fetch('/elpirata/save', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(usuarioData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Respuesta del servidor:', data);
+            
+             // Actualizar la página
+            window.location.reload();
+        })
+        .catch(error => console.error('Error al realizar la solicitud:', error));
+
+        // Cerrar el modal después de realizar acciones
+        miModal.style.display = 'none';
     });
 });
